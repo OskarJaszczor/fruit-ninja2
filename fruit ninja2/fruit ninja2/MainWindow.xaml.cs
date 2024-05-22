d7fe94ed17155ed4b971cc611faa15fd56da9193
OskarJaszczor <oskarjaszczor@gmail.com>
2024-05-22 23:17:21 +0200

    1
    1

---
+++ fruit ninja2/fruit ninja2/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace fruit_ninja2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<Rectangle> rectangles = new List<Rectangle>();
+        Random random = new Random();
+        int gameScore = 0;
+
+        public void CreateAndMovementFruit()
+        {
+            Thread owoc = new Thread(() =>
+            {
+                double width = random.Next(50, 100);
+                double height = random.Next(50, 100);
+
+
+                double beginingHorizontalVelocity = random.Next(5, 10);
+                double beginingVerticalVelocity = -random.Next(20, 25);
+                double gravity = 9.8;
+                double time = 0;
+
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    Rectangle r = new Rectangle();
+
+                    SolidColorBrush fillColor = new SolidColorBrush(Color.FromRgb((byte)random.Next(0, 255), (byte)random.Next(0, 255), (byte)random.Next(0, 255)));
+                    Point position = new Point(random.Next(0, (int)this.Width) - 100, random.Next(500, 550));
+                    Vector velocity = new Vector(0, 0); // Możesz dostosować prędkość początkową wertykalną
+                    r.Width = width;
+                    r.Height = height;
+                    r.Fill = fillColor;
+                    Canvas.SetLeft(r, random.Next(0, (int)this.Width) - 100);
+                    Canvas.SetTop(r, random.Next(500, 550));
+                    //MyRectangle myRectangle = new MyRectangle(width, height, fillColor, position, velocity);
+                    //rectangles.Add(myRectangle);
+                    rectangles.Add(r);
+                    myCanvas.Children.Add(r);
+
+
+                    while (true)
+                    {
+                        Thread.Sleep(20);
+
+                        double x = Canvas.GetLeft(r);
+                        double y = Canvas.GetTop(r);
+
+                        double newX = x + beginingHorizontalVelocity * time;
+                        double newY = y + (beginingVerticalVelocity * time) + (0.5 * gravity * time * time);
+
+                        Canvas.SetLeft(r, newX);
+                        Canvas.SetTop(r, newY);
+
+                        time += 0.1;
+                    }
+                }));
+
+            });
+
+            owoc.Start();
+
+        }
+
+        public void FruitMovement(Rectangle r)
+        {
+
+        }
+
+        public void FruitSplit(Rectangle r)
+        {
+            gameScore++;
+            score.Text = gameScore.ToString();
+
+        }
+
+        public MainWindow()
+        {
+
+            for(int i = 0; i < 2; i++)
+            {
+                CreateAndMovementFruit();
+            }
+            
+
+        }
+
+        private void myCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var canvas = sender as Canvas;
+            HitTestResult hitTest = VisualTreeHelper.HitTest(canvas, e.GetPosition(canvas));
+            var element = hitTest.VisualHit;
+
+            if (element.GetType() == typeof(Rectangle))
+            {
+                FruitSplit((Rectangle)element);
+            }
+        }
+
+        private void myCanvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void myCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
